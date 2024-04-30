109e5fef969149e4ccaaec0865769206a6d2d587
YaroslavHunt <yaroslavohotskiy@gmail.com>
2024-04-30 10:18:06 +0300

    Add lesson 1.html Add lesson_1.js Complete homework.

---
+++ scripts/lesson_1.js
@@ -0,0 +1,51 @@
+let hello = 'hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+let one = 1;
+let ten = 10;
+let negative = -999;
+let numbers = 123;
+let pi = 3.14;
+let doubleNum = 2.7;
+let sixteen = 16;
+let yes = true;
+let no = false;
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(one);
+console.log(ten);
+console.log(negative);
+console.log(numbers);
+console.log(pi);
+console.log(doubleNum);
+console.log(sixteen);
+console.log(yes);
+console.log(no);
+
+let firstName = 'Yaroslav';
+let middleName = 'Volodimirovich';
+let lastName = 'Okhotskii';
+let result = `${lastName} ${firstName} ${middleName}`;
+console.log(result);
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let name = prompt('Enter your name:');
+let midName = prompt('Enter your middle name:');
+let age = prompt('Enter your age:');
+console.log(name);
+console.log(midName);
+console.log(age);
+
+
+
+
+
